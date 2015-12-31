4e89a76a280e63bd2248e693c6e3e6d96f6cf3ef
maples <androidskip@gmail.com>
2015-12-31 17:54:44 +0800

    Greendao自动生成代码

---
+++ dev_data/src/main/java/com/android/dev/data/DBLoader.java
@@ -0,0 +1,16 @@
+package com.android.dev.data;
+
+/**
+ * Created with Android Studio.
+ * Project:Android_Dev
+ * Title:DBLoader
+ * Description:
+ * Copyright:Copyright (c) 2014
+ * Company:中国平安健康保险有限公司
+ * Author:mahuiyang
+ * Date:15/12/31
+ * Time:17:45
+ * Version 1.0
+ */
+public class DBLoader {
+}
--- green_dao_gen/src/main/java/com/android/dev/data/db/DaoGenerator.java
+++ green_dao_gen/src/main/java/com/android/dev/data/db/DaoGenerator.java
@@ -1,5 +1,50 @@
 package com.android.dev.data.db;
 
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
 public class DaoGenerator {
+
+    public static void main (String[] args) {
+        String pkg = DaoGenerator.class.getPackage().getName();
+        Schema schema = new Schema(1000, pkg);
+        createLocationTable(schema);
+        createPostTable(schema);
+        try {
+            new de.greenrobot.daogenerator.DaoGenerator().generateAll(schema,
+                    "/Users/mahuiyang/DevTools/StudioProjects/Android_Dev/dev_data/src/main/java-gen");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *  创建帖子信息表
+     * @param schema
+     */
+    private static void createPostTable (Schema schema) {
+        Entity entity = schema.addEntity("PostInfo");
+        entity.addIdProperty();
+        entity.addStringProperty("content");//描述内容
+        entity.addIntProperty("rate");//评价级别
+        entity.addLongProperty("locationId");//场地位置ID
+        entity.addLongProperty("createTime");//创建时间
+        entity.addLongProperty("updateTime");//更新时间
+    }
+
+    /**
+     *  创建场地/位置表
+     * @param schema
+     */
+    private static void createLocationTable (Schema schema) {
+        Entity entity = schema.addEntity("LocationInfo");
+        entity.addIdProperty();//ID
+        entity.addLongProperty("longitude");//经度
+        entity.addLongProperty("latitude");//纬度
+        entity.addStringProperty("name");//场所名称
+        entity.addLongProperty("createTime");//创建时间
+        entity.addLongProperty("updateTime");//更新时间
+    }
+
 
 }
